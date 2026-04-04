018e0c86590f7d5b67d274f69eaf2e65811ce192
undefinedButFine <251344091+undefinedButFine@users.noreply.github.com>
2026-04-04 13:40:38 -0400

    Adding main.py and readme.md description

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Inventory-Manager-Demo
+
+This is a simple text-based inventory management. 
+It is being utilized as a way to practice Python.
---
+++ main.py
@@ -0,0 +1,14 @@
+print('''Welcome to Inventory Manager!
+Please enter your selection''')  #''' matches indent
+
+while True:
+    print(''' 
+        1. Search for item
+        2. Add Item
+        3. Remove Item
+        4. Update Item Price
+        5. Exit
+    ''')
+
+    user_choice = int(input())
+    
