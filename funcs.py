c908c67558debcf2409bf52a7fb0e33024d41763
NextVersionSelf <251344091+NextVersionSelf@users.noreply.github.com>
2026-04-23 07:05:01 -0400

    Add validation func to prevent numeric inputs + itemSearch() validation in funcs.py

--- funcs.py
+++ funcs.py
@@ -1,13 +1,17 @@
 def itemSearch():
     searchTarget = input('Enter search item: ')
 
-    with open("items.txt", "r") as file:
-        for item in file:
-            if searchTarget.lower().strip() == item.lower().strip(): #Removes whitespace otherwise won't find
-                print(f"\n{item.strip()} has been found.")
-                break
-        else:
-            print("Item was not found!\n")
+    try: 
+        validateInputString(searchTarget)
+        with open("items.txt", "r") as file:
+            for item in file:
+                if searchTarget.lower().strip() == item.lower().strip(): #Removes whitespace otherwise won't find
+                    print(f"\n{item.strip()} has been found.")
+                    break
+            else:
+                print("Item was not found!\n")
+    except ValueError as e:
+        print(e)
 
 def itemAdd():
     addTarget = input('Please enter the name of the item you wish to add: ')
@@ -28,3 +32,13 @@ def itemRemove():
         file.writelines(items) #Rewrite over old items
         file.truncate() #Remove everything at/after the current pointer position (last new list)
         print(f"Removed {removeTarget.strip()} successfully.\n")
+
+
+
+def validateInputString(value): #Prevent numbers from being entered, assuming combination is not number+characters
+    try:                        #.isnumeric() will return false for the same reasons above
+        float(value)            #https://www.w3schools.com/python/ref_string_isnumeric.asp
+    except ValueError:
+        return
+    
+    raise ValueError("Numeric inputs are not allowed.\n")
